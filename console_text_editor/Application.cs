d7a48400d260f3b1d3cf1263b96745813e1f8f3d
Rinkachirukuki <belkossquirrel@gmail.com>
2021-05-13 19:34:28 +0500

    Initial commit

---
+++ console_text_editor/Application.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_text_editor
+{
+    class Application
+    {
+        public void ExecuteCommand(ICommand command)
+        {
+            command.Execute();
+        }
+
+
+
+
+    }
+}
